3912955a867bb9df2f51b40027cf2d7e88cf3c37
unknown <bhambrikhushi4@gmail.com>
2022-01-20 11:08:43 +0530

    files added

---
+++ dataCleaning.py
@@ -0,0 +1,74 @@
+import re
+import pandas as pd
+
+def split_user(x):
+    x=x.split(': ',1)
+    if x[1:]:
+        return x
+    else:
+        return(['group notification',x[0]])
+
+def Reset_User(x):
+    pat = '\+\d{2}\s\d{5}\s\d{5}'
+    if(re.fullmatch(pat,x)):
+        return x[4:]
+    else:
+        return x
+    
+def  processText(data):
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\W{0,1}\w{0,2}\s-\s'
+    messages = re.split(pattern,data)[1:]
+    dates=re.findall(pattern[:-3],data)
+    
+    df=pd.DataFrame({'Date':dates,'Messages':messages})
+    
+    df['User']=df['Messages'].apply(lambda x: split_user(x)[0])
+    df['Messages']= df['Messages'].apply(lambda x: split_user(x)[1][:-1])    
+    df['User']=df['User'].apply(lambda x: Reset_User(x))
+    
+    df['Datetime']=pd.to_datetime(df['Date'])
+    df['Date']=df['Date'].apply(lambda x: x.split(', ')[0])
+    
+    mfirst=0
+    dfirst=0
+    for d in dates:
+        x=d.split('/',2)[0:2]
+        if(int(x[0])>12):
+            dfirst=1
+            break
+        if(int(x[1])>12):
+            mfirst=1
+            break
+    
+    if(dfirst):
+        df['Date']=pd.to_datetime(df['Date'],format='%d/%m/%y')
+    else:
+        df['Date']=pd.to_datetime(df['Date'],format='%m/%d/%y')
+           
+    df['Date']=pd.to_datetime(df['Date']) # format('%d/%m/%y, %H:%M %p')   
+    
+    df['Day']=df['Date'].dt.day
+    df['Month']=df['Date'].dt.month_name()
+    df['Year']=df['Date'].dt.year
+    df['Month_num'] = df['Date'].dt.month
+    df['Week']=df['Date'].dt.isocalendar().week
+    df['Day_name'] = df['Date'].dt.day_name()
+    df['Time']=df['Datetime'].dt.time
+    df['Hour'] = df['Datetime'].dt.hour
+    df['Minute'] = df['Datetime'].dt.minute
+    
+    df.drop('Datetime',axis='columns')
+   
+    return df
+
+
+def Remove_StopWords(x,links):
+    f=open('StopWords.txt','r')
+    stopWords= f.read().split("\n")
+    links=" ".join(links)
+    y = []
+    if x!="This message was deleted" and x!="<Media omitted>" and x!='You deleted this message':
+        for word in x.split():
+            if word.lower() not in stopWords and word not in links:
+                y.append(word)
+    return " ".join(y)
