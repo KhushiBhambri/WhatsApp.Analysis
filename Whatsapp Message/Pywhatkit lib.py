8101ff435ff5c7d85b2d11105cd751b4dfc98f01
unknown <bhambrikhushi4@gmail.com>
2022-01-24 23:33:37 +0530

    .

---
+++ Whatsapp Message/Pywhatkit lib.py
@@ -0,0 +1,25 @@
+from datetime import date
+import pywhatkit as kit
+# kit.text_to_handwriting("hi i m khushi. how are ?")
+# kit.search("Gaming")
+# kit.sendwhatmsg("+918427796214","hello this is my mannat.",20,21)
+today = date.today()
+d2 = today.strftime("%B %d, %Y")
+kit.playonyt(" harimandar sahib today" + d2)
+
+
+# # dd/mm/YY
+# d1 = today.strftime("%d/%m/%Y")
+# print("d1 =", d1)
+
+# # Textual month, day and year
+# d2 = today.strftime("%B %d, %Y")
+# print("d2 =", d2)
+
+# # mm/dd/y
+# d3 = today.strftime("%m/%d/%y")
+# print("d3 =", d3)
+
+# # Month abbreviation, day and year
+# d4 = today.strftime("%b-%d-%Y")
+# print("d4 =", d4)
